51421bf6f846fc6e5d0a6bde3f0fcd12f2388b05
trial1ph2 <155462210+trial1ph2@users.noreply.github.com>
2024-02-19 15:10:23 +0000

    script updated

--- scripts/load_python_env.sh
+++ scripts/load_python_env.sh
@@ -4,4 +4,4 @@ echo 'Creating Python virtual environment "scripts/.venv"...'
 python3 -m venv scripts/.venv
 
 echo 'Installing dependencies from "requirements.txt" into virtual environment (in quiet mode)...'
-./scripts/.venv/bin/python -m pip --quiet --disable-pip-version-check install -r scripts/requirements.txt
+./scripts/.venv/bin/python -m pip --quiet --disable-pip-version-check install -r requirements.txt
--- scripts/retrain.py
+++ scripts/retrain.py
@@ -1,6 +1,12 @@
 import subprocess, sys
 
-p = subprocess.Popen(["powershell.exe", 
-              ".\\prepdocs.ps1"], 
-              stdout=sys.stdout)
-p.communicate()
+# p = subprocess.Popen(["sh", "-c", "source prepdocs.sh"], 
+#               stdout=sys.stdout)
+# p.communicate()
+
+print(subprocess.run(["./prepdocs.sh", 
+                "arguments"], shell=True))
+
+
+# import os
+# os.popen('sh prepdocs.sh')
